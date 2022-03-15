4e51bb128ffcf2721589c99c7752f4a8e9525459
Vaibhav5702 <65025018+Vaibhav5702@users.noreply.github.com>
2022-03-15 16:11:48 +0530

    Time: 49 ms (17.64%), Space: 53.5 MB (14.60%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.java
+++ 76-minimum-window-substring/76-minimum-window-substring.java
@@ -1,72 +1,66 @@
 class Solution {
-  public String minWindow(String s, String t) {
+    public String minWindow(String s, String t) {
 
-      if (s.length() == 0 || t.length() == 0) {
-          return "";
-      }
+        if (s.length() == 0 || t.length() == 0) {
+            return "";
+        }
 
-      // Dictionary which keeps a count of all the unique characters in t.
-      Map<Character, Integer> dictT = new HashMap<Character, Integer>();
-      for (int i = 0; i < t.length(); i++) {
-          int count = dictT.getOrDefault(t.charAt(i), 0);
-          dictT.put(t.charAt(i), count + 1);
-      }
+        Map<Character, Integer> dictT = new HashMap<Character, Integer>();
 
-      // Number of unique characters in t, which need to be present in the desired window.
-      int required = dictT.size();
+        for (int i = 0; i < t.length(); i++) {
+            int count = dictT.getOrDefault(t.charAt(i), 0);
+            dictT.put(t.charAt(i), count + 1);
+        }
 
-      // Left and Right pointer
-      int l = 0, r = 0;
+        int required = dictT.size();
 
-      // formed is used to keep track of how many unique characters in t
-      // are present in the current window in its desired frequency.
-      // e.g. if t is "AABC" then the window must have two A's, one B and one C.
-      // Thus formed would be = 3 when all these conditions are met.
-      int formed = 0;
+        // Filter all the characters from s into a new list along with their index.
+        // The filtering criteria is that the character should be present in t.
+        List<Pair<Integer, Character>> filteredS = new ArrayList<Pair<Integer, Character>>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (dictT.containsKey(c)) {
+                filteredS.add(new Pair<Integer, Character>(i, c));
+            }
+        }
 
-      // Dictionary which keeps a count of all the unique characters in the current window.
-      Map<Character, Integer> windowCounts = new HashMap<Character, Integer>();
+        int l = 0, r = 0, formed = 0;
+        Map<Character, Integer> windowCounts = new HashMap<Character, Integer>();  
+        int[] ans = {-1, 0, 0};
 
-      // ans list of the form (window length, left, right)
-      int[] ans = {-1, 0, 0};
+        // Look for the characters only in the filtered list instead of entire s.
+        // This helps to reduce our search.
+        // Hence, we follow the sliding window approach on as small list.
+        while (r < filteredS.size()) {
+            char c = filteredS.get(r).getValue();
+            int count = windowCounts.getOrDefault(c, 0);
+            windowCounts.put(c, count + 1);
 
-      while (r < s.length()) {
-          // Add one character from the right to the window
-          char c = s.charAt(r);
-          int count = windowCounts.getOrDefault(c, 0);
-          windowCounts.put(c, count + 1);
+            if (dictT.containsKey(c) && windowCounts.get(c).intValue() == dictT.get(c).intValue()) {
+                formed++;
+            }
 
-          // If the frequency of the current character added equals to the
-          // desired count in t then increment the formed count by 1.
-          if (dictT.containsKey(c) && windowCounts.get(c).intValue() == dictT.get(c).intValue()) {
-              formed++;
-          }
+            // Try and contract the window till the point where it ceases to be 'desirable'.
+            while (l <= r && formed == required) {
+                c = filteredS.get(l).getValue();
 
-          // Try and contract the window till the point where it ceases to be 'desirable'.
-          while (l <= r && formed == required) {
-              c = s.charAt(l);
-              // Save the smallest window until now.
-              if (ans[0] == -1 || r - l + 1 < ans[0]) {
-                  ans[0] = r - l + 1;
-                  ans[1] = l;
-                  ans[2] = r;
-              }
+                // Save the smallest window until now.
+                int end = filteredS.get(r).getKey();
+                int start = filteredS.get(l).getKey();
+                if (ans[0] == -1 || end - start + 1 < ans[0]) {
+                    ans[0] = end - start + 1;
+                    ans[1] = start;
+                    ans[2] = end;
+                }
 
-              // The character at the position pointed by the
-              // `Left` pointer is no longer a part of the window.
-              windowCounts.put(c, windowCounts.get(c) - 1);
-              if (dictT.containsKey(c) && windowCounts.get(c).intValue() < dictT.get(c).intValue()) {
-                  formed--;
-              }
-
-              // Move the left pointer ahead, this would help to look for a new window.
-              l++;
-          }
-
-          // Keep expanding the window once we are done contracting.
-          r++;   
-      }
-
-      return ans[0] == -1 ? "" : s.substring(ans[1], ans[2] + 1);
-  }
+                windowCounts.put(c, windowCounts.get(c) - 1);
+                if (dictT.containsKey(c) && windowCounts.get(c).intValue() < dictT.get(c).intValue()) {
+                    formed--;
+                }
+                l++;
+            }
+            r++;   
+        }
+        return ans[0] == -1 ? "" : s.substring(ans[1], ans[2] + 1);
+    }
 }
