a86dd467f349a812a1f5a3f5ac69de5a0227c867
Vaibhav5702 <65025018+Vaibhav5702@users.noreply.github.com>
2022-03-22 22:49:34 +0530

    Time: 596 ms (76.01%), Space: 123.4 MB (48.91%) - LeetHub

--- 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.java
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.java
@@ -1,20 +1,16 @@
 class Solution {
-    public int minStoneSum(int[] piles, int k) {
-        PriorityQueue<Integer> pq=new PriorityQueue(Collections.reverseOrder());
-        for(int pile:piles)
-        {
-            pq.offer(pile);
+    public int minStoneSum(int[] A, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b)->b - a);
+        int res = 0;
+        for (int a : A) {
+            pq.add(a);
+            res += a;
         }
-        for(int i=1;i<=k;i++)
-        {
-            int a=pq.poll();
-            pq.offer(a-a/2);
+        while (k-- > 0) {
+            int a = pq.poll();
+            pq.add(a - a / 2);
+            res -= a / 2;
         }
-        int sum=0;
-        while(!pq.isEmpty())
-        {
-            sum+=pq.poll();
-        }
-        return sum;
+        return res;
     }
 }
