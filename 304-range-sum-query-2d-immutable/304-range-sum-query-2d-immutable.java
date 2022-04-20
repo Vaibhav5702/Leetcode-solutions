a99ec6cad117e3e4a4d60ba1e455e26866052816
Vaibhav5702 <65025018+Vaibhav5702@users.noreply.github.com>
2022-04-20 19:10:39 +0530

    Time: 225 ms (23.43%), Space: 132.1 MB (31.25%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.java
@@ -21,14 +21,14 @@ class NumMatrix {
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        if(row1==0&&col1==0)
-            return preSum[row2][col2];
-        else if(row1==0)
+        if(row1!=0&&col1!=0)
+            return preSum[row2][col2]+preSum[row1-1][col1-1]-preSum[row1-1][col2]-preSum[row2][col1-1];
+        else if(col1!=0)
             return preSum[row2][col2]-preSum[row2][col1-1];
-        else if(col1==0)
+        else if(row1!=0)
             return preSum[row2][col2]-preSum[row1-1][col2];
         else
-            return preSum[row2][col2]+preSum[row1-1][col1-1]-preSum[row1-1][col2]-preSum[row2][col1-1];
+            return preSum[row2][col2];
     }
 }
 
