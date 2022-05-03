314ae0f6ce6c1be21035962dbccc88117f0ff94e
Vaibhav5702 <65025018+Vaibhav5702@users.noreply.github.com>
2022-05-03 13:34:21 +0530

    Time: 48 ms (12.96%), Space: 43.2 MB (80.72%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.java
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.java
@@ -1,35 +1,18 @@
-class Solution {
+public class Solution {
     public int findUnsortedSubarray(int[] nums) {
-        Stack<Integer> stack=new Stack();
-        int l=Integer.MAX_VALUE;
-        for(int i=0;i<nums.length;i++)
-        {
-            if(stack.isEmpty()||nums[i]>=stack.peek())
-            {
-                stack.push(nums[i]);
-                continue;
-            }
-            while(!stack.isEmpty()&&nums[i]<stack.peek())
-            {
-                stack.pop();
-            }
-            l=Math.min(l,stack.size());
-            stack.push(nums[i]);
+        Stack < Integer > stack = new Stack < Integer > ();
+        int l = nums.length, r = 0;
+        for (int i = 0; i < nums.length; i++) {
+            while (!stack.isEmpty() && nums[stack.peek()] > nums[i])
+                l = Math.min(l, stack.pop());
+            stack.push(i);
         }
-        stack=new Stack();
-        int r=Integer.MIN_VALUE;
-        for(int i=nums.length-1;i>=0;i--)
-        {
-            if(stack.isEmpty()||nums[i]<=stack.peek())
-            {
-                stack.push(nums[i]);
-                continue;
-            }
-            while(!stack.isEmpty()&&nums[i]>stack.peek())
-                stack.pop();
-            r=Math.max(r,nums.length-stack.size()-1);
-            stack.push(nums[i]);
+        stack.clear();
+        for (int i = nums.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && nums[stack.peek()] < nums[i])
+                r = Math.max(r, stack.pop());
+            stack.push(i);
         }
-        return l>=r?0:r-l+1;
+        return r - l > 0 ? r - l + 1 : 0;
     }
 }
