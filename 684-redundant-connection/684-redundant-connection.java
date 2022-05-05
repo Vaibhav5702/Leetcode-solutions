67ff80a9270c7219643d0a74961823c81aa6885b
Vaibhav5702 <65025018+Vaibhav5702@users.noreply.github.com>
2022-05-05 13:32:58 +0530

    Time: 1 ms (78.88%), Space: 44.4 MB (26.31%) - LeetHub

---
+++ 684-redundant-connection/684-redundant-connection.java
@@ -0,0 +1,42 @@
+class Solution {
+    int[] parent;
+    int[] rank;
+    public int[] findRedundantConnection(int[][] edges) {
+        parent=new int[edges.length+1];
+        rank=new int[edges.length+1];
+        for(int i=0;i<parent.length;i++)
+        {
+            parent[i]=i;
+        }
+        for(int[] edge:edges)
+        {
+            if(findPar(edge[0])==findPar(edge[1]))
+                return edge;
+            union(findPar(edge[0]),findPar(edge[1]));
+        }
+        return new int[]{};
+    }
+    public int findPar(int node)
+    {
+        if(parent[node]==node)
+            return node;
+        parent[node]=findPar(parent[node]);
+        return parent[node];
+    }
+    public void union(int u, int v)
+    {
+        if(rank[u]==rank[v])
+        {
+            parent[v]=u;
+            rank[u]++;
+        }
+        else if(rank[u]>rank[v])
+        {
+            parent[v]=u;
+        }
+        else
+        {
+            parent[u]=v;
+        }
+    }
+}
