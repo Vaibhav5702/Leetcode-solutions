8bf31610864401cbdda8700334fa2d513e12f4b2
Vaibhav5702 <65025018+Vaibhav5702@users.noreply.github.com>
2022-05-05 13:11:52 +0530

    Time: 2 ms (56.63%), Space: 42.9 MB (37.07%) - LeetHub

--- 399-evaluate-division/399-evaluate-division.java
+++ 399-evaluate-division/399-evaluate-division.java
@@ -11,37 +11,52 @@ class Solution {
             return v + " " + w;
         }
     }
-    
-    public double dfs(String src, String dest, HashSet<String> vis){
-        vis.add(src);
-        if(src.equals(dest)) return 1;
-        double ans = 1;
-        for(Pair e : graph.get(src)){
-            if(!vis.contains(e.v)){
-                double ret = dfs(e.v, dest, vis);
-                if(ret > 0) return e.w * ret;
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        for(int i=0;i<equations.size();i++)
+        {
+            String a1=equations.get(i).get(0);
+            String a2=equations.get(i).get(1);
+            if(!graph.containsKey(a1))
+            {
+                ArrayList<Pair> list=new ArrayList();
+                graph.put(a1,list); 
+            }
+            graph.get(a1).add(new Pair(a2,values[i]));
+            if(!graph.containsKey(a2))
+            {
+                ArrayList<Pair> list=new ArrayList();
+                graph.put(a2,list); 
+            }
+            graph.get(a2).add(new Pair(a1,1/values[i]));
+        }
+        double[] ans=new double[queries.size()];
+        for(int i=0;i<queries.size();i++)
+        {
+            List<String> query=queries.get(i);
+            if(!graph.containsKey(query.get(0))||!graph.containsKey(query.get(1)))
+            {
+                ans[i]=-1;
+                continue;
+            }
+            Set<String> set=new HashSet();
+            ans[i]=dfs(query.get(0),set,query.get(1));
+        }
+        return ans;
+    }
+    public double dfs(String num,Set<String> set,String den)
+    {
+        set.add(num);
+        if(num.equals(den))
+            return 1;
+        for(Pair edge:graph.get(num))
+        {
+            if(!set.contains(edge.v))
+            {
+                double ret=dfs(edge.v,set,den);
+                if(ret>0)
+                    return edge.w*ret;
             }
         }
         return -1.0;
-    }
-    
-    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-        double[] qAnswer = new double[queries.size()];
-        for(int i = 0 ; i < equations.size() ; i++){
-            String u = equations.get(i).get(0), v = equations.get(i).get(1);
-            double w = values[i];
-            graph.putIfAbsent(u, new ArrayList<>());
-            graph.putIfAbsent(v, new ArrayList<>());
-            graph.get(u).add(new Pair(v, w));
-            graph.get(v).add(new Pair(u, 1/w));
-        }
-        
-        for(int i = 0 ; i < queries.size() ; i++){
-            String u = queries.get(i).get(0), v = queries.get(i).get(1);
-            double q = graph.containsKey(u) ? dfs(u, v, new HashSet<>()) : -1.0;
-            qAnswer[i] = q;
-        }
-        
-        return qAnswer;
     }
 }
