8a3db31aeb238d1180038ec8f2bbc436061bb4e3
Vaibhav5702 <65025018+Vaibhav5702@users.noreply.github.com>
2022-05-06 16:42:44 +0530

    Time: 44 ms (49.17%), Space: 48.5 MB (69.66%) - LeetHub

--- 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.java
+++ 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.java
@@ -1,42 +1,44 @@
 class Solution {
+    int[] parent;
     public int removeStones(int[][] stones) {
-        Map<int[],List<int[]>> map=new HashMap();
+        parent=new int[stones.length];
         for(int i=0;i<stones.length;i++)
         {
-            map.put(stones[i],new ArrayList<>());
+            parent[i]=i;
         }
         for(int i=0;i<stones.length;i++)
         {
-            
             for(int j=i+1;j<stones.length;j++)
             {
                 if(stones[i][0]==stones[j][0]||stones[i][1]==stones[j][1])
                 {
-                    map.get(stones[i]).add(stones[j]);
-                    map.get(stones[j]).add(stones[i]);
+                    union(i,j);
                 }
             }
         }
-        Set<int[]> set=new HashSet();
         int count=0;
-        for(int[] stone:stones)
+        for(int i=0;i<stones.length;i++)
         {
-            if(!set.contains(stone))
-            {
+            if(parent[i]!=i)
                 count++;
-                dfs(stone,set,map);
-            }
-            
         }
-        return stones.length-count;
+        return count;
     }
-    public void dfs(int[] stone,Set<int[]> set,Map<int[],List<int[]>> map)
+    public int findPar(int node)
     {
-        set.add(stone);
-        for(int[] s:map.get(stone))
+        if(parent[node]!=node)
         {
-            if(!set.contains(s))
-                dfs(s,set,map);
+            parent[node]=findPar(parent[node]);
+        }
+        return parent[node];
+    }
+    public void union(int i, int j)
+    {
+        i=findPar(i);
+        j=findPar(j);
+        if(i!=j)
+        {
+            parent[j]=i;
         }
     }
 }
