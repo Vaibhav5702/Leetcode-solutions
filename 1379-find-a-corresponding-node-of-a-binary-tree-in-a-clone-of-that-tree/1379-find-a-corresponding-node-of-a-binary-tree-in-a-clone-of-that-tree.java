e52838464044e4ce0a80a6966eea2da4fd9a476a
Vaibhav5702 <65025018+Vaibhav5702@users.noreply.github.com>
2022-05-17 13:49:14 +0530

    Time: 2 ms (73.36%), Space: 101.3 MB (57.26%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.java
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.java
@@ -10,31 +10,15 @@
 
 class Solution {
     public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
-        Queue<TreeNode> org=new LinkedList();
-        Queue<TreeNode> cl=new LinkedList();
-        org.offer(original);
-        cl.offer(cloned);
-        while(!org.isEmpty())
-        {
-            int size=org.size();
-            for(int i=0;i<size;i++)
-            {
-                TreeNode node1=org.poll();
-                TreeNode node2=cl.poll();
-                if(target==node1)
-                    return node2;
-                if(node1.left!=null)
-                {
-                    org.offer(node1.left);
-                    cl.offer(node2.left);
-                }
-                if(node1.right!=null)
-                {
-                    org.offer(node1.right);
-                    cl.offer(node2.right);
-                }
-            }
-        }
-        return null;
+        if(original==null)
+            return null;
+        if(original==target)
+            return cloned;
+        TreeNode result=null;
+        if(original.left!=null)
+            result=getTargetCopy(original.left,cloned.left,target);
+        if(result==null&&original.right!=null)
+            result=getTargetCopy(original.right,cloned.right,target);
+        return result;
     }
 }
