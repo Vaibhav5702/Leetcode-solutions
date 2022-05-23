78b8c23d8191340a9671847b2dba11f3de117727
Vaibhav5702 <65025018+Vaibhav5702@users.noreply.github.com>
2022-05-23 14:18:45 +0530

    Time: 17 ms (58.64%), Space: 43.3 MB (29.57%) - LeetHub

---
+++ 677-map-sum-pairs/677-map-sum-pairs.java
@@ -0,0 +1,66 @@
+class MapSum {
+
+    class TrieNode{
+        TrieNode[] links=new TrieNode[26];
+        int val=0;
+        public boolean containsKey(char ch)
+        {
+            return links[ch-'a']!=null;
+        }
+        public void set(char ch)
+        {
+            links[ch-'a']=new TrieNode();
+        }
+        public TrieNode get(char ch)
+        {
+            return links[ch-'a'];
+        }
+        public void setEnd(int val)
+        {
+            this.val=val;
+        }
+    }
+    TrieNode root;
+    public MapSum() {
+        root=new TrieNode();
+    }
+    
+    public void insert(String key, int val) {
+        TrieNode node=root;
+        for(char ch:key.toCharArray())
+        {
+            if(!node.containsKey(ch))
+                node.set(ch);
+            node=node.get(ch);
+        }
+        node.setEnd(val);
+    }
+    
+    public int sum(String prefix) {
+        TrieNode node=root;
+        for(char ch:prefix.toCharArray())
+        {
+            if(!node.containsKey(ch))
+                return 0;
+            node=node.get(ch);
+        }
+        return dfs(node);
+    }
+    public int dfs(TrieNode node)
+    {
+        int sum=0;
+        for(char i='a';i<='z';i++)
+        {
+            if(node.containsKey(i))
+                sum+=dfs(node.get(i));
+        }
+        return sum+node.val;
+    }
+}
+
+/**
+ * Your MapSum object will be instantiated and called as such:
+ * MapSum obj = new MapSum();
+ * obj.insert(key,val);
+ * int param_2 = obj.sum(prefix);
+ */
