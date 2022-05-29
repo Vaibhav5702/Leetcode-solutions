c0dbf5318adcea6e00cc9afc9e83edb51bd5e3bb
Vaibhav5702 <65025018+Vaibhav5702@users.noreply.github.com>
2022-05-30 00:11:28 +0530

    Time: 9 ms (84.71%), Space: 47.6 MB (44.57%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.java
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.java
@@ -1,33 +1,26 @@
 class Solution {
     public int maxProduct(String[] words) {
-        boolean[][] freq=new boolean[words.length][26];
+        int[] binary=new int[words.length];
         int max=0;
         for(int i=0;i<words.length;i++)
         {
+            int val=0;
             for(char ch:words[i].toCharArray())
             {
-                freq[i][ch-'a']=true;
+                val|=1<<(ch-'a');
             }
+            binary[i]=val;
         }
         for(int i=0;i<words.length;i++)
         {
             for(int j=i+1;j<words.length;j++)
             {
-                if(check(i,j,freq))
+                if((binary[i]&binary[j])==0)
                 {
                     max=Math.max(max,words[i].length()*words[j].length());
                 }
             }
         }
         return max;
-    }
-    public boolean check(int i,int j, boolean[][] freq)
-    {
-        for(int k=0;k<26;k++)
-        {
-            if(freq[i][k]&&freq[j][k])
-                return false;
-        }
-        return true;
     }
 }
